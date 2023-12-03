ca9a79b4cb7026420c80e1ae56f40cc5615dc2dc
EmpireDemocratiqueDuPoulpe <alexislecomte.pro@protonmail.com>
2023-12-03 21:06:44 +0100

    WIP: Styling

--- src/components/Box/BoxHeader/BoxHeader.types.ts
+++ src/components/Box/BoxHeader/BoxHeader.types.ts
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import type { Property } from "csstype";
 
 /// --- PropTypes ------------------------------------------------------------------------------------------------------
 export type Props = {
